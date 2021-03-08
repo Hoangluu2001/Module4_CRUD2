bfbefb895499fbb38e8d2f9a641ae2b71a1401c0
hoangluu2001 <hoangthehoaluu@gmail.com>
2021-03-08 10:26:57 +0700

    template

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {StudentListComponent} from "./student/student-list/student-list.component";
 import {CreateStudentComponent} from "./student/create-student/create-student.component";
 import {UpdateStudentComponent} from "./student/update-student/update-student.component";
+import {TempalteComponent} from "./tempalte/tempalte.component";
 
 const routes: Routes = [
-  {path: "",component: StudentListComponent , pathMatch:'full'},
+  {path: "student",component: StudentListComponent },
   {path: "add",component: CreateStudentComponent},
   {path: "edit/:id",component: UpdateStudentComponent},
+  {path: "", component:StudentListComponent ,pathMatch:'full'}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NgxPaginationModule} from "ngx-pagination";
 import {Ng2SearchPipeModule} from "ng2-search-filter";
+import { TempalteComponent } from './tempalte/tempalte.component';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import {Ng2SearchPipeModule} from "ng2-search-filter";
     CreateStudentComponent,
     UpdateStudentComponent,
     NavbarComponent,
+    TempalteComponent,
 
   ],
   imports: [
